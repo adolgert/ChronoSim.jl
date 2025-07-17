afeb50a891221b0fd4c9f82c679d2b5f7820d812
adolgert <adolgert@andrew.cmu.edu>
2025-07-17 17:07:12 -0400

    2d works now too

--- src/observed_state.jl
+++ src/observed_state.jl
@@ -25,15 +25,22 @@ Base.eachindex(v::ObservedArray) = eachindex(v.arr)
 Base.iterate(v::ObservedArray) = iterate(v.arr)
 Base.axes(v::ObservedArray) = axes(v.arr)
 
-function Base.getindex(v::ObservedArray, i::Int)
+function Base.getindex(v::ObservedArray{T,1}, i::Int) where T
     element = v.arr[i]
     setfield!(element, :_container, v)
     setfield!(element, :_index, i)
     return element
 end
 
-function Base.getindex(v::ObservedArray, i::Vararg{Int})
+function Base.getindex(v::ObservedArray{T,N}, i::Int) where {T,N}
     element = v.arr[i]
+    setfield!(element, :_container, v)
+    setfield!(element, :_index, Tuple(CartesianIndices(v.arr)[i]))
+    return element
+end
+
+function Base.getindex(v::ObservedArray, i::Vararg{Int})
+    element = v.arr[i...]
     setfield!(element, :_container, v)
     setfield!(element, :_index, i)
     return element
@@ -41,17 +48,24 @@ end
 
 Base.IndexStyle(v::ObservedArray) = Base.IndexStyle(v.arr)
 
-function Base.setindex!(v::ObservedArray, x, i::Int)
+function Base.setindex!(v::ObservedArray{T,1}, x, i::Int) where T
     v.arr[i] = x
     setfield!(x, :_container, v)
     setfield!(x, :_index, i)
     return x
 end
 
-function Base.setindex!(v::ObservedArray, x, i::Vararg{Int})
+function Base.setindex!(v::ObservedArray{T,N}, x, i::Int) where {T,N}
     v.arr[i] = x
     setfield!(x, :_container, v)
-    setfield!(x, :_index, i)
+    setfield!(x, :_index, Tuple(CartesianIndices(v.arr)[i]))
+    return x
+end
+
+function Base.setindex!(v::ObservedArray, x, i::Vararg{Int})
+    v.arr[i...] = x
+    setfield!(x, :_container, v)
+    setfield!(x, :_index, Tuple(CartesianIndices(v.arr)[i]))
     return x
 end
 
--- test/test_observed_state.jl
+++ test/test_observed_state.jl
@@ -5,16 +5,16 @@ mutable struct ObserveContained{T}
     val::Int
     _container::Any
     _index::T
-    ObserveContained{T}(v) where T = new{T}(v)
+    ObserveContained{T}(v) where {T} = new{T}(v)
 end
 
 @testset "ObservedState smoke 1D" begin
     using ChronoSim.ObservedState
-    Contained = ObserveContained{Int}
+    Contained1D = ObserveContained{Int}
     cnt = 32
-    arr = ObservedArray{Contained}(undef, cnt)
+    arr = ObservedArray{Contained1D}(undef, cnt)
     for init in eachindex(arr)
-        arr[init] = Contained(-init)
+        arr[init] = Contained1D(-init)
     end
     for i in eachindex(arr)
         @test getproperty(arr[i], :_index) == i
@@ -26,21 +26,30 @@ end
     @test size(arr) == (cnt,)
 end
 
-
-# @testset "ObservedState smoke 2D" begin
-#     using ChronoSim.ObservedState
-#     Contained = ObserveContained{Int}
-#     dims = (4, 2)
-#     arr = ObservedArray{Contained}(undef, dims...)
-#     for init in eachindex(arr)
-#         arr[init] = Contained(-init)
-#     end
-#     for i in eachindex(arr)
-#         @test getproperty(arr[i], :_index) == i
-#         @test getproperty(arr[i], :_container) == arr
-#         @test arr[i].val == -i
-#     end
-#     @test length(arr) == cnt
-#     @test size(arr, 1) == cnt
-#     @test size(arr) == (cnt,)
-# end
+@testset "ObservedState smoke 2D" begin
+    using ChronoSim.ObservedState
+    Contained2D = ObserveContained{NTuple{2,Int64}}
+    dims = (4, 2)
+    arr = ObservedArray{Contained2D}(undef, dims...)
+    # for init in eachindex(arr)
+    init = 1
+    for idx in CartesianIndices(arr)
+        arr[idx] = Contained2D(-init)
+        init += 1
+    end
+    # This is linear indexing.
+    for i in eachindex(arr)
+        @test typeof(i) == Int64
+        @test getproperty(arr[i], :_container) == arr
+        @test arr[i].val == -i
+    end
+    # This gives you the 2D indices.
+    for idx in CartesianIndices(arr)
+        @test getproperty(arr[idx], :_index) == Tuple(idx)
+    end
+    @test arr[1, 2].val == -5
+    @test length(arr) == dims[1] * dims[2]
+    @test size(arr, 1) == dims[1]
+    @test size(arr, 2) == dims[2]
+    @test size(arr) == dims
+end
