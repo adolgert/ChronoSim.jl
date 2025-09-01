2ec2ec172fb9380a564213b5c6a7b392322db588
adolgert <adolgert@andrew.cmu.edu>
2025-08-31 21:02:30 -0400

    making the dict do primitive types

--- src/ObservedState/observed_dict.jl
+++ src/ObservedState/observed_dict.jl
@@ -5,7 +5,6 @@ mutable struct ObservedDict{K,V} <: AbstractDict{K,V}
     ObservedDict{K,V}(dict) where {K,V} = new{K,V}(dict)
 end
 
-# Constructors
 ObservedDict{K,V}() where {K,V} = ObservedDict{K,V}(Dict{K,V}())
 ObservedDict() = ObservedDict{Any,Any}()
 function ObservedDict(pairs...)
@@ -33,22 +32,44 @@ for op in [:eltype, :haskey, :isempty, :iterate, :keys, :length, :pairs, :size, 
     @eval Base.$op(tv::ObservedDict, args...; kwargs...) = $op(tv.dict, args...; kwargs...)
 end
 
+Base.getindex(v::ObservedDict, i...) = getindex(structure_trait(valtype(v)), v, i...)
 
-function Base.getindex(d::ObservedDict, key)
+function Base.getindex(::PrimitiveTrait, d::ObservedDict, key)
     element = d.dict[key]
-    setfield!(element, :_container, d)
-    setfield!(element, :_index, key)
+    observed_notify(d, key, :read)
     return element
 end
 
-function Base.setindex!(d::ObservedDict, value, key)
+function Base.getindex(::CompoundTrait, d::ObservedDict, key)
+    element = d.dict[key]
+    return _update_index(element, d, key)
+end
+
+function Base.setindex!(v::ObservedDict, value, i...)
+    setindex!(structure_trait(valtype(v)), v, value, i...)
+end
+
+function Base.setindex!(::PrimitiveTrait, d::ObservedDict, value, key)
     d.dict[key] = value
-    setfield!(value, :_container, d)
-    setfield!(value, :_index, key)
+    observed_notify(d, key, :write)
     return value
 end
 
-function Base.delete!(d::ObservedDict, key)
+function Base.setindex!(::CompoundTrait, d::ObservedDict, value, key)
+    d.dict[key] = value
+    return _update_index(value, d, key)
+end
+Base.delete!(v::ObservedDict, i...) = delete!(structure_trait(valtype(v)), v, i...)
+
+function Base.delete!(::PrimitiveTrait, d::ObservedDict, key)
+    if haskey(d.dict, key)
+        delete!(d.dict, key)
+        observed_notify(d, key, :write)
+    end
+    return d
+end
+
+function Base.delete!(::CompoundTrait, d::ObservedDict, key)
     if haskey(d.dict, key)
         element = d.dict[key]
         setfield!(element, :_container, nothing)
@@ -59,18 +80,17 @@ function Base.delete!(d::ObservedDict, key)
     return d
 end
 
+Base.pop!(v::ObservedDict, key) = pop!(structure_trait(valtype(v)), v, key)
 
-function Base.pop!(d::ObservedDict, key)
+function Base.pop!(::PrimitiveTrait, d::ObservedDict, key)
     if haskey(d.dict, key)
-        element = d.dict[key]
-        # missing: notify all fields of this element
-        setfield!(element, :_container, nothing)
+        observed_notify(d, key, :write)
         return pop!(d.dict, key)
     end
     return nothing
 end
 
-function Base.pop!(d::ObservedDict, key, default)
+function Base.pop!(::CompoundTrait, d::ObservedDict, key, default)
     if haskey(d.dict, key)
         element = d.dict[key]
         # missing: notify all fields of this element
@@ -80,7 +100,7 @@ function Base.pop!(d::ObservedDict, key, default)
     return nothing
 end
 
-# popfirest!, poplast!
+# popfirst!, poplast!
 # merge!, empty!, merge
 # similar(d) and similar(d, ::Type{Pair{K,V}})
 # filter!(f, d)
@@ -102,26 +122,32 @@ Base.get!(d::ObservedDict, key, default) =
     end
 
 # Iteration interface
-function Base.iterate(d::ObservedDict)
+Base.iterate(d::ObservedDict) = iterate(structure_trait(valtype(d)), d)
+Base.iterate(d::ObservedDict, state) = iterate(structure_trait(valtype(d)), d, state)
+
+function Base.iterate(::PrimitiveTrait, d::ObservedDict)
     next = iterate(d.dict)
-    if next === nothing
-        return nothing
+    return if next === nothing
+        nothing
     else
-        (k, v), state = next
-        return (k => v), state
+        observed_notify(d, next[1].first, :read)
+        next
     end
 end
 
-function Base.iterate(d::ObservedDict, state)
+Base.iterate(::CompoundTrait, d::ObservedDict) = iterate(d.dict)
+
+function Base.iterate(::PrimitiveTrait, d::ObservedDict, state)
     next = iterate(d.dict, state)
     if next === nothing
         return nothing
     else
-        (k, v), state = next
-        return (k => v), state
+        observed_notify(d, next[1].first, :read)
+        return next
     end
 end
 
+Base.iterate(::CompoundTrait, d::ObservedDict, state) = return iterate(d.dict, state)
 
 function observed_notify(v::ObservedDict, changed, readwrite)
     if isdefined(v, :owner)
